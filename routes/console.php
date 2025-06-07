46ccf8d5288f1e7db3660a5b6ae71c4ec9bea995
iallawi50 <aahh50018@gmail.com>
2025-06-07 11:40:37 +0300

    Scheduling

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Console\Commands\testSchedule;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command(testSchedule::class)->everyMinute();
